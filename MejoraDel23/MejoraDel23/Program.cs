e36c70f4ba1968417b8cbfe89ca4dd1b4d256f0f
aaronsx <senen.00@gmail.com>
2022-12-12 14:42:07 +0100

    Prog

---
+++ MejoraDel23/MejoraDel23/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MejoraDel23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            bool verdadofalso;
+            int pos1 = 0;
+            int pos2 = 0;
+            string persona=string.Empty;
+            string Personareal=string.Empty;
+            List<string> Alumno = new List<string>();
+            string[] Apellidos = {"Sanchez Elegante", "Arenas Matas", "Garcia Salis" };
+            string[] Nombres = {"Alvaro","Antonio","Juan" };
+            Alumno = DevuelveListaGente(Apellidos, Nombres);
+            MuestraColeccion(Alumno, 4);
+            pos1 = CapturaEntero("Introduce la primera posicion que quieres intercambiar", 0, Apellidos.Length - 1);
+            pos2 = CapturaEntero("Introduce la primera posicion que quieres intercambiar", 0, Apellidos.Length - 1);
+            IntercambiaPos(Alumno, pos1, pos2);
+            
+            Console.Write("Introduce la persona que quieres eliminar");
+            persona = Console.ReadLine();
+            for (int i = 0; i < Alumno.Count(); i++)
+            {
+                if (persona == Alumno[i])
+                    Personareal= persona ;
+                else
+                    Console.WriteLine("No existe la persona");
+            }
+            verdadofalso = BorrarElemento(Alumno, Personareal);
+            Pausa("Salir");
+        }
+        
+        static int CapturaEntero(string txt, int min, int max)
+        {
+            bool ok;
+            int num;
+            do { 
+                Console.WriteLine("{0} [{1}...{2}]",txt,min,max);
+                ok=Int32.TryParse(Console.ReadLine(), out num);
+                if(!ok) 
+                {
+                    Console.WriteLine("***ERROR*** Introduce un numero");
+                }
+                else
+                {
+                    if(num<min||num>max)
+                    {
+                        Console.WriteLine("***ERROR*** Introduce un numero que este entre [{0}...{1}]",min,max);
+                        ok = false;
+                    }
+
+                }
+            } while (!ok);
+            return num;
+        }
+        static List<string> DevuelveListaGente(string[] Apellidos, string[] Nombre)
+        {
+            List<string> Alumno = new List<string>();
+            for (int i=0; i<Apellidos.Length;i++)
+            {
+                Alumno.Add(Apellidos[i]+" "+ Nombre[i]);
+            }
+            return Alumno;
+        }
+        static void MuestraColeccion(List<string> Alumno, int colum)
+        {
+            for (int i = 0; i < Alumno.Count(); i++)
+            {
+               Console.SetCursorPosition(colum, i+2);
+                Console.WriteLine(Alumno[i]);
+            }
+            
+        }
+        static void IntercambiaPos(List<string> Alumno, int pos1, int pos2)
+        {
+            string nombre;
+            nombre = Alumno[pos1];
+            Alumno[pos1] = Alumno[pos2];
+            Alumno[pos2] = nombre;
+
+            MuestraColeccion(Alumno, 30);
+
+
+        }
+        static Boolean BorrarElemento(List<string> Alumno, string persona)
+        {
+         
+            for(int i=0; i< Alumno.Count; i++) 
+            {
+                if (persona == Alumno[i])
+                {
+                    Alumno.Remove(persona);
+                    MuestraColeccion(Alumno, 30);
+                    return true;
+                }
+                else
+                    return false;
+                
+            }
+            return false;
+        }
+        static void Pausa(string texto)
+        {
+            Console.WriteLine("\t\t\n\nPulsa una tecla para " + texto);
+            Console.ReadKey(true);
+        }
+
+    }
+}
--- P23Ejemplo2/P23Ejemplo2/Program.cs
+++ P23Ejemplo2/P23Ejemplo2/Program.cs
@@ -10,19 +10,79 @@ namespace P23Ejemplo2
     {
         static void Main(string[] args)
         {
-            List<char> list;
-            int num = 0;
-            num = CapturaEntero("Cuantos elementos deseas cargar", 10, 24);
-            list = new List<char>(num);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            int num;
+            int num2;
+            int posicion;
+            int numeliminar;
+            int posicionEliminar;
+            int numBuscar;
+            int numEncon;
+            List<int> miLista;
+            miLista = new List<int>();
+            num = CapturaEntero("\n\n\t\tIntroduce un numero entre ", 10, 24);
 
-            Pausa("salir");
+            Random random = new Random();
+            for (int i = 0; i < num; i++)
+            {
+                miLista.Add(random.Next(10, 100));
+            }
+
+            MuestraListaEnColumna(miLista, 4, "Original");
+            num2 = CapturaEntero("\n\n\t\tIntroduce un numero entre ", 100, 300);
+            posicion = CapturaEntero("\n\n\t\tIntroduce una posicion ", 0, miLista.Count()-1);
+            miLista.Insert(posicion, num2);
+            MuestraListaEnColumna(miLista, 28, "Modificado");
+
+
+            Console.SetCursorPosition(2, 25);
+            numeliminar = CapturaEntero("\n\n\t\tIntroduce un numero entre para eliminar", 10, 300);
+
+            if (numeliminar == miLista.Count())
+            {
+                miLista.Remove(numeliminar);
+            }
+            else if (numeliminar != miLista.Count())
+            {
+                Console.Write("\n\n\t\tEl numero introducido no existe");
+            }
+
+            Console.SetCursorPosition(2, 30);
+            posicionEliminar = CapturaEntero("\n\n\t\tIntroduce una posicion para eliminar", 0, miLista.Count()- 1);
+            miLista.RemoveAt(posicionEliminar);
+            MuestraListaEnColumna(miLista, 40, "Modificada");
+
+
+
+            Console.SetCursorPosition(2, 35);
+            numBuscar = CapturaEntero("\n\n\t\tIntroduce el numero que quieres buscar", 10, 300);
+            numEncon = miLista.IndexOf(numBuscar);
+
+            if (numEncon > 0)
+                Console.WriteLine("\n\n\t El numero {0} se encuntra en {1}", numBuscar, numEncon);
+            else
+                Console.WriteLine("\n\n\t El numero {0} no se encuentra en la lista", numBuscar);
+            Pausa("Pulse cualquier letra para salir");
+
         }
-        static void MuestraListaEnColumna(List<char> lista)
+        static void MuestraListaEnColumna(List<int> miLista, int columna, string txt)
         {
-            
+            int cont = 8;
+            Console.SetCursorPosition(columna, 5);
+            Console.Write("{0}", txt);
+            for (int i = 0; i < miLista.Count(); i++)
+            {
+                Console.SetCursorPosition(columna, cont);
+                cont++;
+                Console.Write("{0}) {1}", i, miLista[i]);
+            }
         }
-        
-        
+        static void Pausa(string txt)
+        {
+            Console.SetCursorPosition(2, 40);
+            Console.Write(txt);
+            Console.ReadKey(true);
+        }
         static int CapturaEntero(string texto, int min, int max)
         {
             bool esCorrecto;
@@ -41,11 +101,7 @@ namespace P23Ejemplo2
                 }
             } while (!esCorrecto);
             return valor;
-        }
-        static void Pausa(string txt)
-        {
-            Console.Write("\n\n\t\tPulse cualquier tecla para {0}", txt);
-            Console.ReadKey(true);
+
         }
     }
 }
